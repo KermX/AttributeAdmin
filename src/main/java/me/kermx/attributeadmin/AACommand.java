3c130e36d33b1c6e3d6a3a194d5ac4e3c602c01a
KermX <KermitVIII@gmail.com>
2023-11-13 23:26:48 -0600

    Initial commit

---
+++ src/main/java/me/kermx/attributeadmin/AACommand.java
@@ -0,0 +1,2 @@
+package me.kermx.attributeadmin;public class AACommand {
+}
---
+++ src/main/java/me/kermx/attributeadmin/AttributeAdmin.java
@@ -0,0 +1,17 @@
+package me.kermx.attributeadmin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AttributeAdmin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
